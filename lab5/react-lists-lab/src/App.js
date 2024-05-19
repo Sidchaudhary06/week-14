189185b022fcaaf6fc8c62441f419b7f32630958
SIDDHANT <siddhant23111990@gmail.com>
2024-05-19 13:19:13 +0300

    week-14

---
+++ lab5/react-lists-lab/src/App.js
@@ -0,0 +1,15 @@
+import Book from './Book.js';
+import booksData from './booksData.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Book List</h1>
+      <div className="book-list">
+  {booksData.map(book => <Book key={book.id}  book={book} name="sami"/>)} 
+      </div>
+    </div>
+  );
+}
+
+export default App;
